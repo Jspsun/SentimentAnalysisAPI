09d81453232a28d7f98a0992a0d2f0b80f9491ae
Jspsun <Jspsun@gmail.com>
2017-03-12 04:34:46 -0400

    seperate branch

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 // Bring in our dependencies
-const app = require('express')();
+var express=require('express')();
+var app = express();
 const routes = require('./routes');
 
 
+app.set('port', (process.env.PORT || 5001));
+// app.use(express.static(__dirname));
 
 // Connect all our routes to our application
 app.use('/', routes);
 
 // Turn on that server!
-app.listen(8001, () => {
-  console.log('App listening on port 3000');
-
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
