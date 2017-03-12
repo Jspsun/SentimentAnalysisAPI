c1a1eb49aa9854bfaaa9a275b777b5e4662c3740
Jspsun <Jspsun@gmail.com>
2017-03-12 04:47:23 -0400

    fixed server issues

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-// Bring in our dependencies
-var express=require('express')();
+var express = require('express');
 var app = express();
 const routes = require('./routes');
 
-
 app.set('port', (process.env.PORT || 5001));
-// app.use(express.static(__dirname));
 
-// Connect all our routes to our application
 app.use('/', routes);
 
-// Turn on that server!
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
